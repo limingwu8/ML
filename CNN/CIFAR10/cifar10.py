d7688d668aac6003772f57bfa569e5056d4ba38a
liming wu <limingwu8@gmail.com>
2017-10-01 00:29:15 -0500

    use deep network

--- CNN/CIFAR10/cifar10.py
+++ CNN/CIFAR10/cifar10.py
@@ -1,18 +1,11 @@
 from keras.datasets import cifar10
-import numpy as np
-from PIL import Image
-import os
-import matplotlib.pyplot as plt
-import pandas as pd
-from keras.utils import np_utils
-from sklearn.model_selection import train_test_split
-
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPool2D
+from keras.layers import Conv2D, MaxPooling2D, GlobalMaxPooling2D
 from keras.optimizers import SGD, RMSprop, adam
 from keras.utils import np_utils
 from keras.optimizers import Adam
+from keras.callbacks import TensorBoard
 
 (X_train, y_train),(X_test, y_test) = cifar10.load_data()
 y_train = np_utils.to_categorical(y_train,10)
@@ -22,34 +15,48 @@ print("X_test shape:%s, y_test shape:%s"%(X_test.shape,y_test.shape))
 
 model = Sequential()
 
-model.add(Conv2D(
-    filters = 32,
-    kernel_size = 5,
-    strides = 1,
-    padding = 'same',
-    input_shape = (32,32,3)
-))
+model.add(Conv2D(32, (3, 3), padding='same',input_shape=X_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(48, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(48, (3, 3), padding='same'))
 model.add(Activation('relu'))
 
-model.add(MaxPool2D(
-    pool_size=(2, 2),
-    strides=(2, 2),
-    padding='same'
-))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
 
-model.add(Conv2D(
-    filters = 64,
-    kernel_size = 5,
-    strides = 1,
-    padding = 'same',
-))
+model.add(Conv2D(80, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(80, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(80, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(80, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(80, (3, 3), padding='same'))
 model.add(Activation('relu'))
 
-model.add(MaxPool2D(
-    pool_size=(2, 2),
-    strides=(2, 2),
-    padding='same'
-))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(128, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(128, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(128, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(128, (3, 3), padding='same'))
+model.add(Activation('relu'))
+
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
 # Dense Layer
 model.add(Flatten())
 model.add(Dense(units=1024))
@@ -67,7 +74,12 @@ model.compile(optimizer=adam,
 
 # train network
 print('Training...')
-model.fit(X_train,y_train,batch_size=20,epochs=2)
+tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0,
+                          write_graph=True, write_images=False)
+model.fit(X_train,y_train,
+          batch_size=32,
+          epochs=200,
+          callbacks=[tensorboard])
 loss,accuracy = model.evaluate(X_test,y_test)
 print("\nloss:",loss)
 print("accuracy:",accuracy)
