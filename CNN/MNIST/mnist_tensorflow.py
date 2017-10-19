235b1d6babbd0ca0cb9cfd1a910fbe6292c527a8
liming wu <limingwu8@gmail.com>
2017-10-19 10:49:04 -0500

    MNIST

--- CNN/MNIST/mnist_tensorflow.py
+++ CNN/MNIST/mnist_tensorflow.py
@@ -57,7 +57,7 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    for i in range(2000):
+    for i in range(1000):
         batch = mnist.train.next_batch(50)
         if i % 100 == 0:
             train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
