4c95ab738e5fe79e75a15450f39fbd999c844dba
wu1114 <limingwu8@gmail.com>
2017-11-02 17:15:14 -0500

    LSTM prediction

--- RNN/short_term_prediction/sensor-one-step-prediction.py
+++ RNN/short_term_prediction/sensor-one-step-prediction.py
@@ -14,79 +14,81 @@ import numpy as np
 
 # date-time parsing function for loading the dataset
 def parser(x):
-	return datetime.strptime('190'+x, '%Y-%m')
+    return datetime.strptime('190'+x, '%Y-%m')
 
 # frame a sequence as a supervised learning problem
 def timeseries_to_supervised(data, lag=1):
-	df = DataFrame(data)
-	columns = [df.shift(i) for i in range(1, lag+1)]
-	columns.append(df)
-	df = concat(columns, axis=1)
-	df.fillna(0, inplace=True)
-	return df
+    df = DataFrame(data)
+    columns = [df.shift(i) for i in range(1, lag+1)]
+    columns.append(df)
+    df = concat(columns, axis=1)
+    df.fillna(0, inplace=True)
+    return df
 
 # create a differenced series
 def difference(dataset, interval=1):
-	diff = list()
-	for i in range(interval, len(dataset)):
-		value = dataset[i] - dataset[i - interval]
-		diff.append(value)
-	return Series(diff)
+    diff = list()
+    for i in range(interval, len(dataset)):
+        value = dataset[i] - dataset[i - interval]
+        diff.append(value)
+    return Series(diff)
 
 # invert differenced value
 def inverse_difference(history, yhat, interval=1):
-	return yhat + history[-interval]
+    return yhat + history[-interval]
 
 # scale train and test data to [-1, 1]
 def scale(train, test):
-	# fit scaler
-	scaler = MinMaxScaler(feature_range=(-1, 1))
-	scaler = scaler.fit(train)
-	# transform train
-	train = train.reshape(train.shape[0], train.shape[1])
-	train_scaled = scaler.transform(train)
-	# transform test
-	test = test.reshape(test.shape[0], test.shape[1])
-	test_scaled = scaler.transform(test)
-	return scaler, train_scaled, test_scaled
+    # fit scaler
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    scaler = scaler.fit(train)
+    # transform train
+    train = train.reshape(train.shape[0], train.shape[1])
+    train_scaled = scaler.transform(train)
+    # transform test
+    test = test.reshape(test.shape[0], test.shape[1])
+    test_scaled = scaler.transform(test)
+    return scaler, train_scaled, test_scaled
 
 # inverse scaling for a forecasted value
 def invert_scale(scaler, X, value):
-	new_row = [x for x in X] + [value]
-	array = np.array(new_row)
-	array = array.reshape(1, len(array))
-	inverted = scaler.inverse_transform(array)
-	return inverted[0, -1]
+    new_row = [x for x in X] + [value]
+    array = np.array(new_row)
+    array = array.reshape(1, len(array))
+    inverted = scaler.inverse_transform(array)
+    return inverted[0, -1]
 
 # fit an LSTM network to training data
 def fit_lstm(train, batch_size, nb_epoch, neurons):
-	X, y = train[:, 0:-1], train[:, -1]
-	X = X.reshape(X.shape[0], 1, X.shape[1])
-	model = Sequential()
-	model.add(LSTM(neurons, batch_input_shape=(batch_size, X.shape[1], X.shape[2]), stateful=True))
-	model.add(Dense(1))
-	model.compile(loss='mean_squared_error', optimizer='adam')
-	for i in range(nb_epoch):
-		model.fit(X, y, epochs=1, batch_size=batch_size, verbose=0, shuffle=False)
-		model.reset_states()
-	return model
+    X, y = train[:, 0:-1], train[:, -1]
+    X = X.reshape(X.shape[0], 1, X.shape[1])
+    model = Sequential()
+    model.add(LSTM(neurons, batch_input_shape=(batch_size, X.shape[1], X.shape[2]), stateful=True))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    for i in range(nb_epoch):
+        model.fit(X, y, epochs=1, batch_size=batch_size, verbose=0, shuffle=False)
+        model.reset_states()
+    return model
 
 # make a one-step forecast
 def forecast_lstm(model, batch_size, X):
-	X = X.reshape(1, 1, len(X))
-	yhat = model.predict(X, batch_size=batch_size)
-	return yhat[0,0]
+    X = X.reshape(1, 1, len(X))
+    yhat = model.predict(X, batch_size=batch_size)
+    return yhat[0,0]
 
-TRAIN_LEN = 400
 
 # load dataset
-series = read_csv('PT-204.csv', header=1)
+series = read_csv('./dataset/OIL_RETURN_TEMP_unsample.csv',sep=',')
+raw_time = series['Time']
+raw_values = series['Value']
+raw_time = raw_time.values
+raw_values = raw_values.values
+
+train_len = int(len(raw_values)*0.8)
+test_len = int(len(raw_values)*0.2)
 
 # transform data to be stationary
-raw_values = series.values
-raw_values = raw_values.reshape(raw_values.shape[0])
-np.dtype([('raw_values', np.float32)])
-
 diff_values = difference(raw_values, 1)
 
 # transform data to be supervised learning
@@ -94,37 +96,41 @@ supervised = timeseries_to_supervised(diff_values, 1)
 supervised_values = supervised.values
 
 # split data into train and test-sets
-train, test = supervised_values[:TRAIN_LEN], supervised_values[TRAIN_LEN:]
+train, test = supervised_values[0:-test_len], supervised_values[-test_len:]
 
 # transform the scale of the data
 scaler, train_scaled, test_scaled = scale(train, test)
 
 # fit the model
-lstm_model = fit_lstm(train_scaled, 1, 1, 4)
+lstm_model = fit_lstm(train_scaled, 1, 10, 4)
 # forecast the entire training dataset to build up state for forecasting
 train_reshaped = train_scaled[:, 0].reshape(len(train_scaled), 1, 1)
 lstm_model.predict(train_reshaped, batch_size=1)
 
 # walk-forward validation on the test data
 predictions = list()
+error = 0
 for i in range(len(test_scaled)):
-	# make one-step forecast
-	X, y = test_scaled[i, 0:-1], test_scaled[i, -1]
-	yhat = forecast_lstm(lstm_model, 10, X)
-	# invert scaling
-	yhat = invert_scale(scaler, X, yhat)
-	# invert differencing
-	yhat = inverse_difference(raw_values, yhat, len(test_scaled)+1-i)
-	# store forecast
-	predictions.append(yhat)
-	expected = raw_values[len(train) + i]
-	print('timePoint=%d, Predicted=%f, Expected=%f' % (i+1, yhat, expected))
+    # make one-step forecast
+    X, y = test_scaled[i, 0:-1], test_scaled[i, -1]
+    yhat = forecast_lstm(lstm_model, 1, X)
+    # invert scaling
+    yhat = invert_scale(scaler, X, yhat)
+    # invert differencing
+    yhat = inverse_difference(raw_values, yhat, len(test_scaled)+1-i)
+    # store forecast
+    predictions.append(yhat)
+    expected = raw_values[len(train) + i + 1]
+    print('timePoint=%d, Predicted=%f, Expected=%f' % (i+1, yhat, expected))
+    error = error + pow(expected,yhat,2)
 
 # report performance
-rmse = sqrt(mean_squared_error(raw_values[TRAIN_LEN+1:], predictions))
+rmse = sqrt(mean_squared_error(raw_values[-test_len:], predictions))
 print('Test RMSE: %.3f' % rmse)
-# line plot of observed vs predicted
-pyplot.plot(range(0,len(raw_values)),raw_values)
-# pyplot.plot(raw_values[TRAIN_LEN+1:])
-pyplot.plot(range(TRAIN_LEN+1,len(raw_values)),predictions)
+pyplot.plot(raw_values[-test_len:])
+pyplot.plot(predictions)
+# # line plot of observed vs predicted
+# pyplot.plot(range(0,len(raw_values)),raw_values)
+# # pyplot.plot(raw_values[TRAIN_LEN+1:])
+# pyplot.plot(range(train_len+1,len(raw_values)),predictions)
 pyplot.show()
