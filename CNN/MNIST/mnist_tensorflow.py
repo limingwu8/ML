ac253589bc776d9540c8a71d9211c2f944be0575
Liming Wu <limingwu8@gmail.com>
2017-12-16 08:33:15 -0600

    MNIST

--- CNN/MNIST/mnist_tensorflow.py
+++ CNN/MNIST/mnist_tensorflow.py
@@ -55,12 +55,22 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1),tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
 
+# add summary to tensorboard
+tf.summary.scalar('accuracy', accuracy)
+tf.summary.scalar('cross_entropy',cross_entropy)
+merged = tf.summary.merge_all()
+
 with tf.Session() as sess:
+
+    train_writer = tf.summary.FileWriter('./logs/train/', sess.graph)
     sess.run(tf.global_variables_initializer())
+
     for i in range(1000):
         batch = mnist.train.next_batch(50)
         if i % 100 == 0:
-            train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+            # train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+            summary, train_accuracy = sess.run([merged,accuracy], feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+            train_writer.add_summary(summary, i)
             print('step %d, training accuracy %g' % (i, train_accuracy))
         train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
     print('test accuracy %g' % accuracy.eval(feed_dict={
